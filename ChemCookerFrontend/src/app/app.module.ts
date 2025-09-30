d00b1ca066ee7db80a88c26295f838f692a7c41a
Taba-flatbrains <flatbrains.taba@gmail.com>
2025-09-30 13:49:46 +0200

    search sieht jetzt ganz gut aus

--- ChemCookerFrontend/src/app/app.module.ts
+++ ChemCookerFrontend/src/app/app.module.ts
@@ -13,8 +13,9 @@ import { ReactiveFormsModule } from '@angular/forms';
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import {MatButtonModule} from '@angular/material/button';
     MatFormFieldModule, 
     MatInputModule, 
     MatSelectModule, BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
 ],
   providers: [],
   bootstrap: [AppComponent]
