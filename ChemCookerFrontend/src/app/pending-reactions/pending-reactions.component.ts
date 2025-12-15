cb2e0a6f130d5f65672345be1b422c63767d6cff
Taba-flatbrains <henni_fuchs@yahoo.de>
2025-12-15 14:30:56 +0100

    now has border around newly unlocked chems in succesful pending reactions

--- ChemCookerFrontend/src/app/pending-reactions/pending-reactions.component.ts
+++ ChemCookerFrontend/src/app/pending-reactions/pending-reactions.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ChemicalsService } from '../chemical/chemicals.service';
+import { CookResponse } from '../util/backend.service';
 
 @Component({
   selector: 'app-pending-reactions',
   templateUrl: './pending-reactions.component.html',
   styleUrl: './pending-reactions.component.css'
 })
-export class PendingReactionsComponent {
+export class PendingReactionsComponent implements OnInit {
   constructor(public chemService:ChemicalsService) { }
 
   tempToString : { [id:number] : string} = { 1 : "Cold", 10 : "rt. ", 100: "Reflux", 1000 : "pyro"}
+
+  succesfulReactions : SuccesfulPendingReaction[] = [];
+  newChemStyle = {'border-color': 'yellow', 'border-width': '2px', 'border-style': 'solid'};
+  ngOnInit(): void {
+    this.succesfulReactions = this.chemService.successfulPendingReactions.map( r => {
+      return {
+        ...r,
+        products_whighlighting: r.products.map( p => { return { smile: p.smile, new: r.new_chems.map(i => {return i.smile}).includes(p.smile) }; } )
+      }
+    });
+  }
+}
+
+export interface SuccesfulPendingReaction extends CookResponse {
+  products_whighlighting: { smile: string, new: boolean }[];
 }
